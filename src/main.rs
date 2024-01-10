c343b31e0f17abb120096fedfb21ff6e382efb5b
syamimhazmi <syamimhazmi@gmail.com>
2024-01-11 01:06:17 +0700

    if else

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+fn main() {
+    // let number = 3;
+    let number = 7;
+
+    // if number < 5 {
+    //     println!("condition was true");
+    // } else {
+    //     println!("condition was false");
+    // }
+
+    // Condition must be a bool
+    // This code will not run, since we dont provide a comparison value
+    // if number {
+    //     println!("The number is seven");
+    // }
+
+    if number != 0 {
+        println!("number was something other than zero");
+    }
+}
