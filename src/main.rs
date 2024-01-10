d8b450cc73dd5211249671e949e43f7311277e87
syamimhazmi <syamimhazmi@gmail.com>
2024-01-11 01:31:52 +0700

    example countdown using loops

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,7 @@
 fn main() {
-    // let a = [10, 20, 30, 40, 50, 60];
-
-    // let mut index = 0;
-
-    // while index < 5 {
-    //     println!("The value is: {}", a[index]);
-
-    //     index += 1;
-    // }
-
-    // Another way to use for loop
-    let a = [10, 20, 30, 40, 50, 60];
-
-    for element in a {
-        println!("The value is: {element}");
+    for number in (1..4).rev() {
+        println!("{number}");
     }
+
+    println!("LiftOFF!");
 }
