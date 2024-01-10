42cdb515523bbbdded871cb01310b8d8b60af93b
syamimhazmi <syamimhazmi@gmail.com>
2024-01-11 01:15:37 +0700

    repeating with loops

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,13 @@
 fn main() {
-    // Multiple if else
-    // let number = 6;
+    let mut counter = 0;
 
-    // if number % 4 == 0 {
-    //     println!("number is divisible by 4");
-    // } else if number % 3 == 0 {
-    //     println!("number is divisible by 3");
-    // } else if number % 2 == 0 {
-    //     println!("number is divisible by 2");
-    // } else {
-    //     println!("number is is not divisible by 2, 3, 4");
-    // }
+    let result = loop {
+        counter += 1;
 
-    // Using if in let statement
-    let condition = true;
-    let number = if condition { 5 } else { 6 };
-    // This will break the code. since rust check the value of variable number is integer instead of string
-    // let number = if condition { 5 } else { "six" };
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
 
-    println!("The number is: {number}");
+    println!("The result is: {result}");
 }
