4a9be304277e81a7a1c1987eb2d024a8acd80292
syamimhazmi <syamimhazmi@gmail.com>
2024-01-11 09:24:02 +0700

    loop using in statement

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,20 @@
 fn main() {
     // This code will be slow since compiler add runtime to check the index value.
+    // let a = [10, 20, 30, 40, 50];
+
+    // let mut index = 0;
+
+    // while index < 5 {
+    //     println!("the value is: {}", a[index]);
+
+    //     index += 1;
+    // }
+
+    // Another alternative to run this without compiler adding runtime,
+    // we use for loop
     let a = [10, 20, 30, 40, 50];
 
-    let mut index = 0;
-
-    while index < 5 {
-        println!("the value is: {}", a[index]);
-
-        index += 1;
+    for element in a {
+        println!("the value is: {element}");
     }
 }
