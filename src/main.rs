737b2d588fc09f0b8d1aebbfb24e1eb1eb6009d7
syamimhazmi <syamimhazmi@gmail.com>
2024-03-05 15:44:48 +0700

    temperature converter

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,47 @@
 fn main() {
-    // let number = 3;
-    let number = 7;
+    loop {
+        println!("Please put the value of temperature that you want to convert.");
 
-    // if number < 5 {
-    //     println!("condition was true");
-    // } else {
-    //     println!("condition was false");
-    // }
+        let mut temperature = String::new();
+        std::io::stdin()
+            .read_line(&mut temperature)
+            .expect("Failed to temperature read line");
 
-    // Condition must be a bool
-    // This code will not run, since we dont provide a comparison value
-    // if number {
-    //     println!("The number is seven");
-    // }
+        let temperature: f64 = temperature.trim()
+            .parse()
+            .expect("Please type a float value. eg: 32.5 or 24.0");
 
-    if number != 0 {
-        println!("number was something other than zero");
+        println!("Please type the value of temperature format that you want to.");
+
+        let mut temperature_format = String::new();
+
+        std::io::stdin()
+            .read_line(&mut temperature_format)
+            .expect("Failed to read temperature format line");
+
+        let temperature_format = temperature_format.trim()
+            .parse()
+            .expect("Failed to parse format string");
+
+        let result = convert(temperature, temperature_format);
+
+        println!("{}", result)
+    }
+
+}
+
+fn convert(temperature: f64, format: String) -> String {
+    match format.to_lowercase().as_str() {
+        "celsius" => {
+            let value = (temperature * (9.0/5.0)) + 32.0;
+            format!("The value from fahrenheit to celsius is: {:.2}°C", value).to_string()
+        },
+        "fahrenheit" => {
+            let value = (temperature - 32.0) * (5.0/9.0);
+            format!("The value from celsius to fahrenheit is: {:.2}°F", value).to_string()
+        },
+        _ => {
+            "Invalid format value".to_string()
+        }
     }
 }
